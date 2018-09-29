3b408cee24571320d7fffedba5a2f752d11daa14
arek-cholewa <arkadiusz.cholewaa@gmail.com>
2018-09-30 00:00:32 +0200

    home endpoint added

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const accessKey = 'pbJpHBZeaChyHyydssok';
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => res.status(200).send({
+    message: 'Welcome to the proxy-codewars API!'
+  }));
+
 app.get('/users/:idOrUsername', (req, res) => {
     fetch(`https://www.codewars.com/api/v1/users/${req.params.idOrUsername}?access_key=${accessKey}`)
         .then(response => response.json())
