4d4d2d4ecbf14641247ce6e8e1256eb8293880e5
arek-cholewa <arkadiusz.cholewaa@gmail.com>
2018-09-29 23:50:36 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const fetch = require("node-fetch");
+const cors = require('cors');
+const port = process.env.PORT || 4000;
+const accessKey = 'pbJpHBZeaChyHyydssok';
+
+const app = express();
+app.use(cors());
+
+app.get('/users/:idOrUsername', (req, res) => {
+    fetch(`https://www.codewars.com/api/v1/users/${req.params.idOrUsername}?access_key=${accessKey}`)
+        .then(response => response.json())
+        .then(response => res.send(response));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
