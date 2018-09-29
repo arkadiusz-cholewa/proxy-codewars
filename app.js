4fa644c5e03037b02479b6503297b9e7996a619b
arek-cholewa <arkadiusz.cholewaa@gmail.com>
2018-09-30 00:17:57 +0200

    test

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ var fetch = require("node-fetch");
 var cors = require('cors');
 var port = process.env.PORT || 3000;
 var app = express();
-var accessKey = 'pbJpHBZeaChyHyydssok';
-
-var app = express();
 app.use(cors());
-
-app.get('/', (req, res) => res.status(200).send({
-    message: 'Welcome to the proxy-codewars API!'
-}));
-
-app.get('/users/:idOrUsername', (req, res) => {
-    fetch(`https://www.codewars.com/api/v1/users/${req.params.idOrUsername}?access_key=${accessKey}`)
+app.get('users/:user', function (req, res) {
+    fetch(`https://www.codewars.com/api/v1/users/${req.params.user}?access_key=pbJpHBZeaChyHyydssok`)
         .then(response => response.json())
         .then(response => res.send(response));
 });
-
 app.listen(port, function () {
     console.log(`Example app listening on port !`);
 });
