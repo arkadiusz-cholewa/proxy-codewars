72161392208fc6f25335f0ae143acfb4aee3f898
arek-cholewa <arkadiusz.cholewaa@gmail.com>
2018-09-30 00:09:49 +0200

    var added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require("node-fetch");
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 const accessKey = 'pbJpHBZeaChyHyydssok';
 
 const app = express();
